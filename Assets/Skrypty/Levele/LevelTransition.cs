52410b89392edae92eaf0049fcd2b3bc5d86b12b
Juzefus998 <73847054+Juzefus998@users.noreply.github.com>
2022-11-26 03:36:07 +0100

    Wielki Powrot
    Transistion

---
+++ Assets/Skrypty/Levele/LevelTransition.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(4));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
