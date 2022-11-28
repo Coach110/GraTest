0426205d9700117c6e44de6c0161f38c2591b6ad
Juzefus998 <73847054+Juzefus998@users.noreply.github.com>
2022-11-28 14:46:17 +0100

    No juz jakos to dziala

---
+++ Assets/FollowPlayerCM.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FollowPlayerCM : MonoBehaviour
+{
+    private CinemachineVirtualCamera vcam; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vcam = GetComponent<CinemachineVirtualCamera>();
+        vcam.Follow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+}
